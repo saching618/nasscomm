b0cb751f48e6258f97a2817de3473e09d55cced9
ratneshkeshari <ratnesh.keshari@box8.in>
2020-01-18 14:58:14 +0530

    depracted comment

--- src/util.js
+++ src/util.js
@@ -9,14 +9,16 @@ const setToken = function(token, email) {
     client.set(token, email)
 }
 
-const validateToken = function(token) {
-    return new Promise((reject, resolve) => {
-        client.get(token, function(err, reply) {
-            if (err) reject('Some thing went wrong!');
-            console.log('fasfjkjdshf');
-            resolve(reply);
-        })  
-    }) 
-}
+
+// Deprecated
+// const validateToken = function(token) {
+//     return new Promise((reject, resolve) => {
+//         client.get(token, function(err, reply) {
+//             if (err) reject('Some thing went wrong!');
+//             console.log('fasfjkjdshf');
+//             resolve(reply);
+//         })  
+//     }) 
+// }
 
 module.exports = {setToken, client}
