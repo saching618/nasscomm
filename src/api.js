a694c595bbdfb5b8c7f650b6cefc1482eae92a16
ratneshkeshari <ratnesh.keshari@box8.in>
2020-01-18 14:50:59 +0530

    validation change

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 
 const util = require('./util')
 const axios = require('axios')
-
+const redisClient = util.client
 function getAccessToken(email) {
   const client = axios.create({
     baseURL: `https://hackapi.reverieinc.com/token?email=${email}`,
@@ -9,14 +9,14 @@ function getAccessToken(email) {
   });
 
   client.get().then(function (response) {
-      util.setToken(response.data['message'])
+      util.setToken(response.data['message'], email)
   })
   .catch(function (error) {
     console.log(error);
   })
 }
 
-function converText(text, tgt, src, token){
+function convertText(text, tgt, src, token){
 
     const raw_data = {
         'data': text,
@@ -33,10 +33,23 @@ function converText(text, tgt, src, token){
       });
 
       client.post('/nmt', raw_data).then(function (response) {
-          console.log(response.data.data)
+          console.log(response.data)
       })
       .catch(function (error) {
         console.log(error);
       })
 }
-converText(['Hi Ramu'], 'hi', 'en', 'e8a79f08677139a4e990a14d511faae0c6c7841b');
+
+
+var token = 'e8a79f08677139a4e990a14d511faae0c6c7841b';
+redisClient.get(token, function(err, reply) {
+  if (err) {
+     console.log('Something went wrong.');
+  }
+  if (reply) {
+    convertText(['hi Hello'],'hi', 'en', token);
+    console.log(reply);
+  } else{
+    console.log("Unauthorized");
+  }
+})  
