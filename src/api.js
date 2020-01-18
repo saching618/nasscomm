b9ff3bf8568d95eb31067cc953f9df185c3d539a
saching618 <sachin.gupta@edureka.co>
2020-01-18 14:25:07 +0530

    convertTExtToaudio api and nodemon

--- src/api.js
+++ src/api.js
@@ -16,7 +16,7 @@ function getAccessToken(email) {
   })
 }
 
-function converText(text, tgt, src, token){
+function convertText(text, tgt, src, token){
 
     const raw_data = {
         'data': text,
@@ -33,10 +33,52 @@ function converText(text, tgt, src, token){
       });
 
       client.post('/nmt', raw_data).then(function (response) {
+          console.log('contentText response')
           console.log(response.data.data)
       })
       .catch(function (error) {
+        console.log('contentText error response')
         console.log(error);
       })
 }
-converText(['Hi Ramu'], 'hi', 'en', 'e8a79f08677139a4e990a14d511faae0c6c7841b');
+// convertText(['Hi Ramu'], 'hi', 'en', 'e8a79f08677139a4e990a14d511faae0c6c7841b');
+
+
+function convertTextToAudio(text, lang, token){
+    const raw_data = {
+        'text': text,
+        'lang': lang,
+    };
+
+    const client = axios.create({
+        baseURL: 'https://hackapi.reverieinc.com',
+        headers: { 
+            'content-type': 'application/json',
+            'token': token
+        }
+      });
+
+      client.post('/tts', raw_data).then(function (response) {
+          console.log('convertTextToAudio response')
+          console.log(response.status)
+          if(response.status ==200){
+            let res = {};
+            res['status'] =response.status;
+            res['data'] =  response.data;
+            console.log(res.data);
+            return res;
+          }else{
+            let res = {};
+            res['status'] =response.status;
+            res['data'] = "error";
+            console.log(res);
+            return res;
+          }
+      })
+      .catch(function (error) {
+        console.log('convertTextToAudio error response')
+        console.log(error);
+      })
+
+}
+convertTextToAudio('[Hello World]', 'en', 'e8a79f08677139a4e990a14d511faae0c6c7841b');
